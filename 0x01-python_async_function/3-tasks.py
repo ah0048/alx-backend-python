2fe54e912d0b1525c2181794dcbc13912ee25d7d
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-15 00:56:06 +0300

    3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,3 +2,10 @@
 '''The basics of async syntax'''
 
 
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Return a asyncio.Task'''
+    return asyncio.create_task(wait_random(max_delay))

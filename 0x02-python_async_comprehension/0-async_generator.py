a17fcd343c7cf0d8b573b84e466e9270b6889098
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-10-15 21:13:58 +0300

    0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,12 +3,12 @@
 
 
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     '''Yields a random number between 0 and 10'''
-    for _ in range(0, 10):
+    for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
